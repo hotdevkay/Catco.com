479e62a737c16160421dfac5a551785157f617a1
hotdevkay <hotdevkay@gmail.com>
2024-08-22 23:43:27 +0100

    Add files via upload

---
+++ bac.bash
@@ -0,0 +1,8 @@
+/catco-website
+    index.html
+    styles.css
+    app.js
+    /images
+        cat-toy.jpg
+        cat-food.jpg
+        cat-bed.jpg
---
+++ document.js
@@ -0,0 +1,3 @@
+document.getElementById("learn-more").addEventListener("click", function() {
+    alert("Welcome to CatCo! We offer the best products for your feline friends.");
+});
